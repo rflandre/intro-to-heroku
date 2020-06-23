c1019c40d672d0112bd2d3794b3c7909d25965f1
rflandre <renaud.flandre@gmail.com>
2020-06-24 00:25:15 +0200

    zdz

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ import {WelcomePage} from './pages/welcome/welcome';
 import {PropertyListPage} from './pages/property-list/property-list';
 import {BrokerListPage} from './pages/broker-list/broker-list';
 import {FavoriteListPage} from './pages/favorite-list/favorite-list';
-//import {ProfilePage} from './pages/profile';
+import {ProfilePage} from './pages/profile';
 import {PropertyService} from './services/property-service';
 import {BrokerService} from './services/broker-service';
 import {ProfileService} from './services/profile-service';
@@ -35,7 +35,7 @@ class MyApp {
             {title: 'Welcome', component: WelcomePage, icon: "bookmark"},
             {title: 'Products', component: PropertyListPage, icon: "pricetag"},
             {title: 'Brand ambassador', component: BrokerListPage, icon: "people"},
-            {title: 'Favorites', component: FavoriteListPage, icon: "star"}
+            {title: 'Favorites', component: FavoriteListPage, icon: "star"},
             {title: 'Profile', component: Profilepage, icon: "person"}
         ];
 
