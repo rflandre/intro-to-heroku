bb67bbab8e0158e709d7e131b26defde6a2be417
rflandre <renaud.flandre@gmail.com>
2020-07-01 23:15:24 +0200

    Revert "Update server.js"
    This reverts commit a019c124c571ee4c439c6038e20fe7edf740432b.

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var brokerTable = 'broker__c';
 
 var storage =   multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './');
+    callback(null, './uploads');
   },
   filename: function (req, file, callback) {
     callback(null, file.fieldname + '-' + Date.now());
